36658688d8105acc70bd282af6942e6a690e8c53
Kamimura27 <e1b18027@oit.ac.jp>
2020-11-10 16:25:50 +0900

    DBの初期設定

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO jinrou (tf, name, syuzoku, jinei, dora) VALUES (FALSE, '人狼', 0, 0, 0);
+INSERT INTO jinrou (tf, name, syuzoku, jinei, dora) VALUES (FALSE, '占い師', 1, 1, 0);
+INSERT INTO jinrou (tf, name, syuzoku, jinei, dora) VALUES (FALSE, '騎士', 1, 1, 0);
+INSERT INTO jinrou (tf, name, syuzoku, jinei, dora) VALUES (FALSE, '平民', 1, 1, 0);
+INSERT INTO jinrou (tf, name, syuzoku, jinei, dora) VALUES (FALSE, '平民', 1, 1, 0);
+INSERT INTO jinrou (tf, name, syuzoku, jinei, dora) VALUES (FALSE, '人狼', 0, 0, 0);
+INSERT INTO jinrou (tf, name, syuzoku, jinei, dora) VALUES (FALSE, '霊媒師', 1, 1, 0);
+INSERT INTO jinrou (tf, name, syuzoku, jinei, dora) VALUES (FALSE, '平民', 1, 1, 0);
+INSERT INTO jinrou (tf, name, syuzoku, jinei, dora) VALUES (FALSE, '狂人', 1, 0, 0);
+INSERT INTO jinrou (tf, name, syuzoku, jinei, dora) VALUES (FALSE, '占い師', 1, 1, 0);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE jinrou(
+  id IDENTITY,
+  username CHAR,
+  tf BOOLEAN NOT NULL,
+  name CHAR NOT NULL,
+  syuzoku INT NOT NULL,
+  jinei INT NOT NULL,
+  dora INT NOT NULL
+);
