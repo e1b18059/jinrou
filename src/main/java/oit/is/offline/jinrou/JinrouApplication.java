ff2eddd62a6a3e0d3392d192e2cf2470392a3fa3
e1b18059 <e1b18059@oit.ac.jp>
2020-11-10 15:23:05 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/offline/jinrou/JinrouApplication.java
@@ -0,0 +1,13 @@
+package oit.is.offline.jinrou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinrouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinrouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/offline/jinrou/JinrouApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.offline.jinrou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinrouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
