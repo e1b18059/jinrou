5f9a5fe49a2970dfcec60d849b25870bdfc6e0d8
Kamimura27 <e1b18027@oit.ac.jp>
2020-12-10 13:47:40 +0900

    投票フォームの完成

--- src/main/java/oit/is/offline/jinrou/controller/Controller1.java
+++ src/main/java/oit/is/offline/jinrou/controller/Controller1.java
@@ -16,12 +16,17 @@ import oit.is.offline.jinrou.service.AsyncRoom;
 import oit.is.offline.jinrou.model.RandomRole;
 import oit.is.offline.jinrou.model.UserMapper;
 import oit.is.offline.jinrou.model.User;
+import oit.is.offline.jinrou.model.Vote;
 
 @Controller
 public class Controller1 {
   int count = 0;
-  int counttime = 0;
-  int[] countUser = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  int[] countUser = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };  //誰が何票投票されたか
+  int votecount = 0;  //投票した人数
+  int num;  //ルームにいる人数
+  String voteduser;  //投票されたユーザー
+  ArrayList<String> vote = new ArrayList<String>();
+
 
   @Autowired
   Room room;
@@ -44,7 +49,7 @@ public class Controller1 {
     RandomRole random = new RandomRole();
     User user = new User();
     int i, ran;
-    int num = room.getUsers().size();
+    num = room.getUsers().size();
 
     count++;
     if (count == num) {
@@ -79,9 +84,7 @@ public class Controller1 {
   @GetMapping("/time")
   public SseEmitter pushCount(Principal prin) {
     String user = "user1";
-    int num = room.getUsers().size();
     String nowUser = prin.getName();
-    counttime++;
     final SseEmitter sseEmitter = new SseEmitter();
     this.acroom.time(sseEmitter, nowUser);
 
@@ -91,9 +94,7 @@ public class Controller1 {
   @GetMapping("/vote")
   public String vote(ModelMap model) {
     int i;
-    int num = room.getUsers().size();
     String username;
-    ArrayList<String> vote = new ArrayList<String>();
     vote = userMapper.getvote();
     for (i = 0; i < num; i++) {
       username = "user" + (i + 1);
@@ -104,65 +105,121 @@ public class Controller1 {
     return "vote.html";
   }
 
+  @GetMapping("/voting")  //1回目の投票
+  public String voting(ModelMap model){
+    Vote voting = new Vote();
+    int death = voting.Voting(vote.size(), countUser); //吊るされるユーザー
+
+    if(death == -1){ //再投票へ
+      votecount++;
+      String username;
+      for (int i = 0; i < num; i++) {
+        username = "user" + (i + 1);
+        if (vote.get(i).equals(username)) {
+          model.addAttribute("user" + (i + 1), username);
+        }
+      }
+
+      if(votecount == vote.size()){  //これにアクセスした最後の人の時にカウントを初期化
+        for(int i = 0; i < num; i++){
+          countUser[i] = 0;
+          }
+        votecount = 0;
+      }
+
+      model.addAttribute("revote", death);
+    }else if(death <= 10){  //吊るす人決定
+      voteduser = "user"+death;
+      model.addAttribute("death", voteduser);
+    }
+    return "vote.html";
+}
+
+  @GetMapping("/revoting")  //再投票
+  public String revoting(ModelMap model){
+    Vote voting = new Vote();
+    int death = voting.Voting(vote.size(), countUser); //吊るされるユーザー
+
+    if(death == -1){
+      model.addAttribute("endvote", death);
+    }else if(death <= 10){
+      voteduser = "user"+death;
+      model.addAttribute("death", voteduser);
+    }
+    return "vote.html";
+}
+
+  @GetMapping("/night")
+  public String night(){
+    System.out.println(voteduser);
+    userMapper.vote(voteduser);
+    if(votecount == vote.size()){  //カウントを初期化
+      for(int i = 0; i < num; i++){
+        countUser[i] = 0;
+      }
+      votecount = 0;
+    }
+    return "night.html";
+  }
+
+
   @GetMapping("/user1")
   public String user1() {
     countUser[0]++;
-    System.out.println(countUser[0]);
-    return "game.html";
+    return "vote.html";
   }
 
   @GetMapping("/user2")
   public String user2() {
     countUser[1]++;
-    System.out.println(countUser[1]);
-    return "game.html";
+    return "vote.html";
   }
 
   @GetMapping("/user3")
   public String user3() {
     countUser[2]++;
-    return "game.html";
+    return "vote.html";
   }
 
   @GetMapping("/user4")
   public String user4() {
     countUser[3]++;
-    return "game.html";
+    return "vote.html";
   }
 
   @GetMapping("/user5")
   public String user5() {
     countUser[4]++;
-    return "game.html";
+    return "vote.html";
   }
 
   @GetMapping("/user6")
   public String user6() {
     countUser[5]++;
-    return "game.html";
+    return "vote.html";
   }
 
   @GetMapping("/user7")
   public String user7() {
     countUser[6]++;
-    return "game.html";
+    return "vote.html";
   }
 
   @GetMapping("/user8")
   public String user8() {
     countUser[7]++;
-    return "game.html";
+    return "vote.html";
   }
 
   @GetMapping("/user9")
   public String user9() {
     countUser[8]++;
-    return "game.html";
+    return "vote.html";
   }
 
   @GetMapping("/user10")
   public String user10() {
     countUser[9]++;
-    return "game.html";
+    return "vote.html";
   }
 }
